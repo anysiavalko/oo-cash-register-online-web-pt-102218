29be1f4d72b9f4987901c112ba8d86424ba14159
Anysia V <anysiavalko@gmail.com>
2018-12-20 16:05:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,12 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     if quantity > 1
-      
+      #putting in a counter with a while loop
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
     else
       @items << title
     end
