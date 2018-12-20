2f51469e016ce82fd170abd6561fb049f104444c
Anysia V <anysiavalko@gmail.com>
2018-12-20 15:50:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     
     @total = @total + price * quantity
-    @total = @last_transaction_amount
+    @last_transaction_amount = @total
   end
   
   def apply_discount
